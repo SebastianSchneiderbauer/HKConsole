f4dd9b9b86ab19c2bea456c1f1d608e435d11e7f
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-05-10 00:22:26 +0200

    update command registration to allow for cheats and a cheatActivationConfig
    cheats have their own flag in the register command. Commands with that flag can only be executed in CHEAT mode, which is activated by a user chosen command and prints a ACTIVATION and DEACTIVATION message on entering it. If a command is a cheat but you are not in CHEAT MODE, you will get a error in the console

---
+++ ConsoleCheatConfig.gd
@@ -0,0 +1,8 @@
+class_name HKConsoleCheatConfig
+
+extends Resource
+
+@export var cheatActivationString: String = "IDKFA"
+@export var splitSymbol: String = "|"
+@export var onActivateString: String = "CHEATS ARE ACTIVATED|this is persistent (excluding a restart), to deactivate run command again|go have fun"
+@export var onDeactivateString: String = "CHEATS ARE DEACTIVATED|this is persistent (excluding a restart), to reactivate run command again"
