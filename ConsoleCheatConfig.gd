bb41271e683756969fb2280d1260a952cc43bc70
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-07-18 13:59:26 +0200

    update art

--- ConsoleCheatConfig.gd
+++ ConsoleCheatConfig.gd
@@ -4,5 +4,5 @@ extends Resource
 
 @export var cheatActivationString: String = "IDKFA"
 @export var splitSymbol: String = "|"
-@export var onActivateString: String = "CHEATS ARE ACTIVATED|this is persistent (excluding a restart), to deactivate run command again|go have fun"
+@export var onActivateString: String = "CHEATS ARE ACTIVATED|this is persistent (excluding a restart), to deactivate run command again"
 @export var onDeactivateString: String = "CHEATS ARE DEACTIVATED|this is persistent (excluding a restart), to reactivate run command again"
